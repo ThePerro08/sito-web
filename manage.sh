601d06a0fb8d83d527f1bf961ec752023aeb1818
Giuseppe Perrone <giuseppe.perrone-cic@ibm.com>
2024-10-26 16:11:37 +0200

    modified file

--- manage.sh
+++ manage.sh
@@ -3,18 +3,19 @@
 CONTAINER=gperrone/pelican
 LOCAL_PORT=8000
 SSH_KEY=.ssh/id_rsa
+CUSTOM_DIR=/mnt/c/Users/GiuseppePerrone/Documents/sito-web
 
 case $1 in
     deploy)
-        docker run -it -v ${PWD}:/site:rw,Z -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY}:Z $CONTAINER github
+        docker run -it -v ${CUSTOM_DIR}:/site:rw,Z -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY}:Z $CONTAINER github
         ;;
     update)
-        docker run -it -v ${PWD}:/site:rw,Z $CONTAINER html && docker run -it -v ${PWD}:/site:rw,Z -p ${LOCAL_PORT}:${LOCAL_PORT} $CONTAINER serve 
+        docker run -it -v ${CUSTOM_DIR}:/site:rw,Z $CONTAINER html && docker run -it -v ${CUSTOM_DIR}:/site:rw,Z -p ${LOCAL_PORT}:${LOCAL_PORT} $CONTAINER serve 
         ;;
     build)
         docker build -t ${CONTAINER} .
         ;;
     *)
-        docker run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER $1
+        docker run -it -v ${CUSTOM_DIR}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER $1
         ;; 
 esac
